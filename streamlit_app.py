62127b1112c9ca81e62174226338dad10237eda9
Jonathan Rodriguez <jonathan.rodriguez.can@gmail.com>
2022-10-26 15:46:08 -0400

    Adding placeholders

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,19 +8,19 @@ features = st.container()
 model_training = st.container()
 
 with header:
-    st.title('Welcome')
+    st.title('Lost Sales Dashboard')
 
 with dataset:
-    st.header('Lost sales DS')
+    st.header('Data set by month')
 
     ls_data = pd.read_csv('data/Monthly_lost_sales.csv')
     st.write(ls_data.head())
 
-    tier_distribution = pd.DataFrame(ls_data['MONTH_START_DATE'].value_counts())
+    tier_distribution = pd.DataFrame(ls_data['TIER'].value_counts())
     st.bar_chart(tier_distribution)
 
 with features:
-    st.header('The features I created')
+    st.header('Reserved')
 
 with model_training: 
-    st.header('Time to traiin the model')
+    st.header('Reserved')
